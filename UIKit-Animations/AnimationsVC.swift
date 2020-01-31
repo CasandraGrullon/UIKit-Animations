27d86cc0ba44769109b5b47c4afe04599408306f
Casandra <casandragrullon@pursuit.org>
2020-01-31 12:37:11 -0500

    created three animations for first view. Now working on second view

--- UIKit-Animations/AnimationsVC.swift
+++ UIKit-Animations/AnimationsVC.swift
@@ -18,9 +18,65 @@ class AnimationsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        view.backgroundColor = .systemPink
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        //scaleAnimation()
+        //pulsatingAnimation()
+        scaleImageAddNewImageAnimation()
+    }
     
+    //MARK: Properties we can animate:
+    /*
+     - alpha
+     - corner radius
+     - position
+     - center
+     - transition
+     - color
+     - shadow
+     - opacity
+     - transform: scale, rotate, translate(move object)
+     */
+    
+    //MARK: Animation Sample 1 --> scale animation (transforming an image size)
+    private func scaleAnimation() {
+        UIView.animate(withDuration: 2.0) { // 0.3 == 1/3 of a second
+            // any transform value of 1.0 represents the view's inital size
+            self.animationView.pursuitLogo.transform = CGAffineTransform(scaleX: 30.0, y: 30.0)
+            //MARK: Animation Sample 2 --> fade animation
+            self.animationView.pursuitLogo.alpha = 0
+        }
+    }
+    //MARK: Animation sample 3 -->
+    private func scaleImageAddNewImageAnimation() {
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: [], animations: {
+            self.animationView.pursuitLogo.transform = CGAffineTransform(scaleX: 30.0, y: 30.0)
+            self.animationView.pursuitLogo.alpha = 0
+        }) { (done) in
+            // called when initial animation is finished
+            UIView.animate(withDuration: 0.5) {
+                self.animationView.swiftLogo.isHidden = false
+                self.animationView.swiftLogo.layer.cornerRadius = self.animationView.swiftLogo.bounds.size.width / 2
+            }
+        }
+    }
+    
+    //MARK: Animation Sample 4 --> pulsating image (scaling small to large back repeatedly)
+    private func pulsatingAnimation() {
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+            // animation block
+            self.animationView.pursuitLogo.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        }) { (done) in
+            // code to be executed after animation is complete
+            // options: 1. reset views values   2. create a next animation
+            UIView.animate(withDuration: 0.3) {
+                self.animationView.pursuitLogo.transform = CGAffineTransform.identity // restores back to original state
+            }
+            
+        }
+    }
 
 }
--- UIKit-Animations/AnimationsView.swift
+++ UIKit-Animations/AnimationsView.swift
@@ -13,17 +13,29 @@ class AnimationsView: UIView {
     public lazy var pursuitLogo: UIImageView = {
         let imageView = UIImageView()
         imageView.image = #imageLiteral(resourceName: "Image")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    public lazy var swiftLogo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "swift-logo")
+        imageView.isHidden = true
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: UIScreen.main.bounds)
+        commonInit()
     }
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        commonInit()
     }
     private func commonInit() {
         pursuitLogoConstraints()
+        swiftLogoConstraints()
     }
     
     private func pursuitLogoConstraints() {
@@ -35,6 +47,17 @@ class AnimationsView: UIView {
             pursuitLogo.centerYAnchor.constraint(equalTo: centerYAnchor),
             pursuitLogo.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5),
             pursuitLogo.heightAnchor.constraint(equalTo: pursuitLogo.widthAnchor)
+        ])
+    }
+    private func swiftLogoConstraints() {
+        addSubview(swiftLogo)
+        swiftLogo.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            swiftLogo.centerXAnchor.constraint(equalTo: centerXAnchor),
+            swiftLogo.centerYAnchor.constraint(equalTo: centerYAnchor),
+            swiftLogo.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5),
+            swiftLogo.heightAnchor.constraint(equalTo: swiftLogo.widthAnchor)
         ])
     }
 }
