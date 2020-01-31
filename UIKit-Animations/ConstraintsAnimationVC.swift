f5c0e92fd03d1914009fafeb762ef9b7bbe64b1c
Casandra <casandragrullon@pursuit.org>
2020-01-31 13:16:19 -0500

    animated button when pressed

--- UIKit-Animations/ConstraintsAnimationVC.swift
+++ UIKit-Animations/ConstraintsAnimationVC.swift
@@ -12,6 +12,8 @@ class ConstraintsAnimationVC: UIViewController {
 
     @IBOutlet weak var pinkBoxYConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var pinkBox: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,8 +23,18 @@ class ConstraintsAnimationVC: UIViewController {
     @IBAction func animateViewUP(_ sender: UIButton) {
         //negative value to move box up
         pinkBoxYConstraint.constant -= 100
+        
+        //preventing the box from leaving the screen bounds
+        //if pinkBox.frame.origin.y < 100 { return }
+        
+        //animate button when pressed
+        UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.0, options: [], animations: {
+            sender.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        }) { (done) in
+            sender.transform = CGAffineTransform.identity
+        }
+        
         //when we change the constraints in iOS and want to show it as an animation --> self.view.layoutIfNeeded
-        //
         UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 5.0, options: [.curveEaseIn], animations: {
             self.view.layoutIfNeeded()
         }, completion: nil)
@@ -31,6 +43,14 @@ class ConstraintsAnimationVC: UIViewController {
     @IBAction func animateViewDOWN(_ sender: UIButton) {
         //positive value to move box down
         pinkBoxYConstraint.constant += 100
+        
+        //if pinkBox.frame.origin.y == 0 { return }
+        
+        UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.0, options: [], animations: {
+            sender.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        }) { (done) in
+            sender.transform = CGAffineTransform.identity
+        }
         
         UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 5.0, options: [.curveEaseOut], animations: {
             self.view.layoutIfNeeded()
