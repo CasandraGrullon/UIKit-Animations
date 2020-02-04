0aadaf59414d3d15fe08bc643c64242995e931ee
Casandra <casandragrullon@pursuit.org>
2020-02-04 12:39:09 -0500

    added property animator to stretch an image

--- UIKit-Animations/AnimationsTabBar.swift
+++ UIKit-Animations/AnimationsTabBar.swift
@@ -24,7 +24,7 @@ class AnimationsTabBar: UITabBarController {
         guard let viewcontroller = constraintsAnimationStoryboard.instantiateViewController(identifier: "ConstraintsAnimationVC") as? ConstraintsAnimationVC else {
             fatalError("could not load ConstraintsAnimationVC")
         }
-        viewcontroller.tabBarItem = UITabBarItem(title: "Constraints Animation", image: UIImage(systemName: "2.circle"), tag: 1)
+        viewcontroller.tabBarItem = UITabBarItem(title: "Constraints", image: UIImage(systemName: "2.circle"), tag: 1)
         return viewcontroller
     }()
     
@@ -34,14 +34,24 @@ class AnimationsTabBar: UITabBarController {
         guard let viewcontroller = transitionsStoryboard.instantiateViewController(identifier: "TransitionsAnimationController") as? TransitionsAnimationController else {
             fatalError("could not load TransitionsAnimations")
         }
-        viewcontroller.tabBarItem = UITabBarItem(title: "Transitions Animation", image: UIImage(systemName: "3.circle"), tag: 2)
+        viewcontroller.tabBarItem = UITabBarItem(title: "Transitions", image: UIImage(systemName: "3.circle"), tag: 2)
+        return viewcontroller
+    }()
+    
+    private var propertyAnimatorVC: PropertyAnimatorController = {
+        let propertyStoryboard = UIStoryboard(name: "PropertyAnimator", bundle: nil)
+        
+        guard let viewcontroller = propertyStoryboard.instantiateViewController(identifier: "PropertyAnimatorController") as? PropertyAnimatorController else {
+            fatalError("could not load PropertyAnimatorController")
+        }
+        viewcontroller.tabBarItem = UITabBarItem(title: "Property", image: UIImage(systemName: "4.circle"), tag: 3)
         return viewcontroller
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        viewControllers = [animationsVC, constraintsVC, transitionsVC]
+        viewControllers = [animationsVC, constraintsVC, transitionsVC, propertyAnimatorVC]
     }
     
 }
---
+++ UIKit-Animations/PropertyAnimatorController.swift
@@ -0,0 +1,35 @@
+//
+//  PropertyAnimatorController.swift
+//  UIKit-Animations
+//
+//  Created by casandra grullon on 2/4/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import UIKit
+
+class PropertyAnimatorController: UIViewController {
+
+    @IBOutlet weak var duckImage: UIImageView!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    //1. create an instance to UIViewPropertyAnimator
+    private var animator: UIViewPropertyAnimator!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //2. instantiate the animator property
+        animator = UIViewPropertyAnimator(duration: 1.5, curve: .easeInOut, animations: {
+            self.duckImage.transform = CGAffineTransform(scaleX: 3.0, y: 1)
+        })
+        
+    }
+    
+    @IBAction func sliderDidChange(_ sender: UISlider) {
+        //3. give animator value to sender value
+        animator.fractionComplete = CGFloat(sender.value)
+    }
+    
+}
